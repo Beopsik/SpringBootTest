bbe42c59acbce6364231ceb4d8171c36fbac0c0a
BeobSik <tjswl756@naver.com>
2021-08-09 16:43:16 +0900

    feat: test by MockBean

--- src/test/java/com/example/springboottest/sample/SampleControllerTest.java
+++ src/test/java/com/example/springboottest/sample/SampleControllerTest.java
@@ -5,10 +5,12 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -18,8 +20,14 @@ public class SampleControllerTest {
     @Autowired
     TestRestTemplate testRestTemplate;
 
+    @MockBean
+    SampleService mockSampleService;
+
     @Test
     public void hello() throws Exception{
-        String result=testRestTemplate.getForObject("/hello", String.class);        assertThat(result).isEqualTo("hello beobsik");
+        when(mockSampleService.getName()).thenReturn("beobsik");
+
+        String result=testRestTemplate.getForObject("/hello", String.class);
+        assertThat(result).isEqualTo("hello beobsik");
     }
 }
